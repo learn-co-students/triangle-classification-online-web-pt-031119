35f735260e49f5f65ffa659655aa3417859930f0
James Cox <jamesncox@yahoo.com>
2019-04-12 15:12:37 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,22 +1,34 @@
 class Triangle
-  attr_reader :a, :b, :c
+  attr_accessor :a, :b, :c
   
   def initialize(a, b, c)
     @a = a
     @b = b 
     @c = c
     
-    if a >= b + c || b >= a + c || c >= a + b
-      raise TriangleError
-    end
+    # if a >= b + c || b >= a + c || c >= a + b
+    #   raise TriangleError
+    # end
     
-    if a <= 0 || b <= 0 || c <= 0 
-      raise TriangleError
-    end
+    # if a <= 0 || b <= 0 || c <= 0 
+    #   raise TriangleError
+    # end
   end
+  
+  # def triangle 
+  #   raise TriangleError if a >= b + c || b >= a + c || c >= a + b 
+  
+  #   if a <= 0 || b <= 0 || c <= 0 
+  #     raise TriangleError
+  #   end
+  # end
 
   
   def kind
+    raise TriangleError if a >= b + c || b >= a + c || c >= a + b 
+  
+    raise TriangleError if a <= 0 || b <= 0 || c <= 0 
+    
     if a == b && a == c
       :equilateral
     elsif a == b || b == c || a == c 
