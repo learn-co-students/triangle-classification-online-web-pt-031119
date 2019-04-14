e7699941b954e716ebfdde12adbefdcc03ab74ef
David Stewart <davidstewart@gmail.com>
2019-04-14 23:32:53 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  attr_accessor :one, :two, :three
+ def initialize(x,y,z)
+   @one = x
+   @two = y
+   @three = z
+ end
+  
+  def kind
+   triangle_test
+    if one == two && two == three
+      :equilateral
+    elsif one == two || two == three || one == three
+      :isosceles
+    else
+      :scalene
+    end
+  
+   end
+ 
+ def triangle_test
+    triangle_test = [(one + two > three), (one + three > two), (two + three > one)]
+     [one, two, three].each { |s| triangle_test << false if s <= 0 }
+       raise TriangleError if triangle_test.include?(false)
+ end
+ 
+ 
+ class TriangleError < StandardError
+    # triangle error code
+  end 
+  
 end
+
+
+
