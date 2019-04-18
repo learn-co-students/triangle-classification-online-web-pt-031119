790c0c5e41f4c0e2f4a58921a92bfa71b060c260
Leeya Davis <leeya463@gmail.com>
2019-04-18 04:08:14 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_reader :s1, :s2, :s3
+
+
+  def initialize(s1, s2, s3)
+    @sides = []
+    @sides += [s1, s2, s3]
+    binding.pry
+  end
+
+  def kind(s1, s2, s3)
+    #conditional that determines which triangle the numbers given equates to & returns said triangle
+    #triangle options are :equilateral, :scalene and :isosceles
+    #:equilateral -> each side is equal
+    if s1 == s2 == s3
+    #  return :equilateral
+    #end
+    #:scalene -> each side is different
+    #:isosceles -> has two equal sides
+    #method should raise custom error is triangle is invalid & violates triangle inequality
+    #triangle inequality -> sum of 2 sides doesn't exceed the 3rd
+  end
+
+  #class TriangleError < StandardError
+  #  def message
+  #    "This is not a valid Triangle."
+  #  end
+  #end
 end
